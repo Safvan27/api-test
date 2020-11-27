b0b0d82fe71ef076353e2579b8d4038259a75125
Safvan <safvanparangodath12@gmail.com>
2020-11-27 14:54:03 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
-//import FacebookLogin from 'react-facebook-login';
+import FacebookLogin from 'react-facebook-login';
+const responseFacebook = (response) => {
+  console.log( "Login Result" ,response);
+}
+const componentClicked=(data)=>{
+  console.warn(data)
+}
 class App extends React.Component{
   render(){
     return(
-      <div><h1>Login with facebook</h1></div>
-
+      <div><h1>Login with facebook</h1>
+      <FacebookLogin
+      appId="1021372718401293"
+      autoLoad={false}
+      fields="name,email,picture"
+      onClick={componentClicked}
+      callback={responseFacebook} />
+      </div>
     )
   }
 }
